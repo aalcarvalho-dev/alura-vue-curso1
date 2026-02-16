2f20216cdf28a9d1d4aa8e7d75d175e6f9cac77f
Antônio Alexandre L. de Carvalho <aalcarvalho@celepar.pr.gov.br>
2026-02-16 16:01:48 -0300

    Aula 05. Alternando telas - Finalizando Curso

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -6,6 +6,7 @@ import BotaoPrincipal from './BotaoPrincipal.vue';
 
 
 export default{
+    name:'SelecionarIngredientes',
     data() {
         return {
             categorias: [] as ICategoria[]
@@ -15,7 +16,7 @@ export default{
         this.categorias = await obterCategorias();
     },
     components:{CardCategoria, BotaoPrincipal},
-    emits:['adicionarIngrediente','removerIngrediente']
+    emits:['adicionarIngrediente','removerIngrediente','buscarReceitas']
 }
 
 </script>
@@ -37,7 +38,7 @@ export default{
         <p class="paragrafo dica">
             *Atenção: consideramos que você tem em casa sal, pimenta e água
         </p>
-        <BotaoPrincipal texto="Buscar receitas!" />
+        <BotaoPrincipal texto="Buscar receitas!" @click="$emit('buscarReceitas')"/>
     </section>
 </template>
 
